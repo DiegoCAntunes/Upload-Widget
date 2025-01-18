dc40034a6d25c030f797270c8da623ca3bc09af4
Diego Antunes <diegocazetta@gmail.com>
2025-01-18 20:28:26 -0300

    Upload de imagem

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,5 +1,7 @@
+import { uploadImage } from '@/app/functions/upload-image'
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
+import { isRight, unwrapEither } from '@/shared/either'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
@@ -9,25 +11,40 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
     {
       schema: {
         summary: 'Upload an image',
-        body: z.object({
-          name: z.string(),
-          password: z.string().optional(),
-        }),
+        consumes: ['multipart/form-data'],
+
         response: {
-          201: z.object({ uploadId: z.string() }),
-          409: z
-            .object({ message: z.string() })
-            .describe('Upload already exists.'),
+          201: z.null().describe('Image uploaded'),
+          400: z.object({ message: z.string() }),
         },
       },
     },
     async (request, reply) => {
-      await db.insert(schema.uploads).values({
-        name: 'teste.jpg',
-        remoteKey: 'teste.jpg',
-        remoteUrl: 'http://asdasd.com',
+      const uploadedFile = await request.file({
+        limits: { fileSize: 1024 * 1024 * 2 }, //2MB
       })
-      return reply.status(201).send({ uploadId: 'teste' })
+
+      if (!uploadedFile) {
+        return reply.status(400).send({ message: 'File is required.' })
+      }
+
+      const result = await uploadImage({
+        fileName: uploadedFile?.filename,
+        contentType: uploadedFile.mimetype,
+        contentStream: uploadedFile.file,
+      })
+
+      if (isRight(result)) {
+        console
+        return reply.status(201).send()
+      }
+
+      const error = unwrapEither(result)
+
+      switch (error.constructor.name) {
+        case 'InvalidFileFOrmat':
+          return reply.status(400).send({ message: error.message })
+      }
     }
   )
 }
